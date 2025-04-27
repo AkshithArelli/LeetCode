bb31988abaa6606e79ef40b5b61784b09dab5a91
AkshithArelli <arelliakshith07@gmail.com>
2025-04-27 11:14:50 +0530

    Update 219. Contains Duplicate II.java

--- 219. Contains Duplicate II.java
+++ 219. Contains Duplicate II.java
@@ -36,6 +36,7 @@ class Solution {
         }
         return false;
 
+     //brute force
         for (int i=0; i<nums.length; i++) {
             for (int j=i+1; j<=i+k && j<nums.length; j++) {
                 if (nums[i] == nums[j]) {
