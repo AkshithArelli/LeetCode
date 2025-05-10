214bd22a6d9ba50ce739dbe2eac34acfc44ec188
AkshithArelli <arelliakshith07@gmail.com>
2025-05-10 15:53:59 +0530

    Create 73. Set Matrix Zeroes.java

---
+++ 73. Set Matrix Zeroes.java
@@ -0,0 +1,58 @@
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+
+
+
+
+        //brute force
+        //T: O(Mn(M+M)), S: O(Mn) for extra arraylist
+        List<Integer> list = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        //T: O(m*n)
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (matrix[i][j] == 0) {
+                    list.add(i);
+                    list.add(j);
+                }
+            }
+        }
+
+        //O(m*n) // if all are zero list of size(m*n) and to iterate t takes m*n
+        for (int i=0; i<list.size(); i+=2) {
+            int row=list.get(i);
+            int column=list.get(i+1);
+            //O(m)
+            for (int j=0; j<matrix[0].length; j++) {
+                matrix[row][j] = 0;
+            }
+            //O(n)
+            for (int k=0; k<matrix.length; k++) {
+                matrix[k][column] = 0;
+            }
+        }
+        //(m*n)*(m+n)
+        //(m*n)*(m*n)*(m+n)
+    }
+}
