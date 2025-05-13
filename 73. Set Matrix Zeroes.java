62cbb447f0d83e5153d0f92fc9986342db03627d
AkshithArelli <arelliakshith07@gmail.com>
2025-05-13 20:15:49 +0530

    Update 73. Set Matrix Zeroes.java

--- 73. Set Matrix Zeroes.java
+++ 73. Set Matrix Zeroes.java
@@ -20,7 +20,59 @@ Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 class Solution {
     public void setZeroes(int[][] matrix) {
 
+       //T: O(m*n) , S:O(1)
+        //not using any extra space, but rather using the first column and row
+        //excluding first row and column, if any other place has zero in it,
+        //update corresponding 1st row and column to zero so that later when again traversing
+        //if the first value of that row or column is zero all the elements in that row or column are changed to zero
+        //**incase if 1st row or column has zero in it we change entire 1st row and column to zero as well, so inorder to track those values we are using boolean variables
+        //this is neccessary because it is possible 1st row or column might not have zero but due to zero values in other rows and columns we update 1st r & c to zero by mistake there is a chance we might update entire r & c to zero, inorder to avoid this we have the boolean vars
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean rowContainsZero = false;
+        boolean colContainsZero = false;
 
+        //in the following loop we make sure to check if 1st row and column has zero 
+        //and also we update 1st row and column values to zero if any of the sub-matrix has zero
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (matrix[i][j] == 0) {
+                    //if 1st row has zero in it change it to true so later we can convert all the values in the row of 1st to 0
+                    if (i == 0) rowContainsZero = true;
+                    //if 1st column has zero in it change it to true so later we can convert all the values in the column of 1st to 0
+                    if (j == 0) colContainsZero = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+    
+        //loop through the submatrix and if zero is present in 1st r or c update corresponding entire r & c values to zero (as 1st r & c is already updated to zero)
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                if (matrix[0][j] ==0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (rowContainsZero) {
+            for (int j=0; j<n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (colContainsZero) {
+            for (int i=0; i<m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+
+
+
+     
      
 
         //little bit time optimised but still not space optimised
