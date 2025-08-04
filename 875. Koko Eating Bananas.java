138790cc473ec6ddcc980ee3a69831dfd4995de8
AkshithArelli <arelliakshith07@gmail.com>
2025-08-04 17:49:24 +0530

    Create 875. Koko Eating Bananas.java

---
+++ 875. Koko Eating Bananas.java
@@ -0,0 +1,69 @@
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+
+
+Solution:
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        //T:O(nlogn) logn for binary search and n as we iterate over n ele to find max and check if canEatInTime and S:O(1)
+        //lower bound is set to one because one banana per hour can satify the condition
+        int minValue = 1;
+        //find the maxValue pile which could be the max bananas could be eaten in an hour
+        int maxValue = 0;
+        for (int pile: piles) {
+            maxValue = Math.max(pile, maxValue);
+        }
+        
+        //cheatSheet:
+        //want minValue: (low<high) ; high=mid; low=mid+1
+        //want maxValue: (low<=high) ; low=mid+1; high=mid-1
+        while (minValue < maxValue) { 
+            
+            int midValue = minValue + (maxValue-minValue)/2;
+
+            if (canEatInTime(piles, h, midValue)) {
+                maxValue = midValue; //keep mid maybe it the minimum
+            } else {
+                minValue = midValue+1; //discard mid and smaller values
+            }
+        }
+        return minValue;
+    }
+
+    private boolean canEatInTime(int[] piles, int h, int val) {
+        int hours = 0;
+        for (int pile: piles) {
+            hours += (int)Math.ceil((double)pile/val);
+        }
+        return hours <=h;
+    }
+}
+  
