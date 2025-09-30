e20514bccf47eaee2714c5376813ed9e7bd48a04
AkshithArelli <arelliakshith07@gmail.com>
2025-09-30 23:41:52 +0530

    Create 206. Reverse Linked List.java

---
+++ 206. Reverse Linked List.java
@@ -0,0 +1,76 @@
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+
+
+Solution:
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        //T: O(n) , S: O(1)
+        if (head == null) {
+            return null;
+        }
+
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode prevNode = null;
+        ListNode currNode = head;
+
+        while (currNode != null) {
+            ListNode nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        head = prevNode;
+        return head;
+
+        //T: O(n) , S: O(n)
+        // Stack<Integer> valueStack = new Stack<>();
+        // while (head != null) {
+        //     valueStack.push(head.val);
+        //     head = head.next;
+        // }
+
+        // //reversedList = minVal -> 
+        // ListNode reversedList = new ListNode(Integer.MIN_VALUE);
+        // ListNode ptr = reversedList;
+        // // minVal ->
+        // //  ptr 
+
+        // while (!valueStack.isEmpty()) {
+        //     ptr.next = new ListNode(valueStack.pop());
+        //     ptr = ptr.next;
+        // }
+
+        // //eg:  1 -> 2 -> 3 -> null
+        // //rev: minVal -> 3 -> 2 -> 1 -> null
+        // //      rev                     ptr
+        // //rev.next:-  3 -> 2 -> 1 -> null
+
+        // return reversedList.next;
+    }
+}
