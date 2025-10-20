1d35f3c31b59c5754fa79a9f12e24787837bbc6c
AkshithArelli <arelliakshith07@gmail.com>
2025-10-20 14:20:52 +0530

    Update 242. Valid Anagram.java

--- 242. Valid Anagram.java
+++ 242. Valid Anagram.java
@@ -27,6 +27,46 @@ Follow up: What if the inputs contain Unicode characters? How would you adapt yo
 
 class Solution {
     public boolean isAnagram(String s, String t) {
+       
+        //T:O(n), S:O(k) -> k=1 if only lower/captial alphabets ortherwise
+                        //  k number of distinct characters stored in map
+        //base condition
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        //put chars to map from s
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        //remove chars from map using t
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c)-1);
+            if (map.get(c)==0) map.remove(c);
+        }
+        return map.isEmpty();
+
+
+
+
+        //note this works only if letters in the words are small alphabets only
+        //base case
+        T: O(n) , S:O(1) as only 26 lower characters
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
+
+        for (int i=0; i<s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+        return true;
+
+     
+     
       //T:O(n) , S:O(n)
         if (s.length() != t.length()) {
             return false;
