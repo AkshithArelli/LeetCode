2914c1b203753c5ae1b2d68827a6f0bf6ccbd045
AkshithArelli <arelliakshith07@gmail.com>
2025-10-31 12:01:46 +0530

    Update 567. Permutation in String.java

--- 567. Permutation in String.java
+++ 567. Permutation in String.java
@@ -24,6 +24,49 @@ s1 and s2 consist of lowercase English letters.
 
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+       /*
+        sliding window + character frequency approach.
+        1.	Count frequency of all chars in s1.
+        2.	Move a window of length = s1.length() across s2.
+        3.	If the frequency of chars in the current window == frequency in s1, return true.
+        4.	Otherwise, slide the window forward by removing the left char and adding the new right char.
+        */
+        //T:O(n),S:O(1)
+        //s1 cannot be greater than s2 
+        //because if s1 is longer, no permutation of s1 can exist in s2 
+
+        if (s1.length() > s2.length())
+            return false;
+
+        int[] s1Freq = new int[26];
+        int[] window = new int[26];
+
+        // Step 1: fill s1 frequency
+        for (char c : s1.toCharArray()) {
+            s1Freq[c - 'a']++;
+        }
+
+        int left = 0;
+        for (int right = 0; right < s2.length(); right++) {
+            window[s2.charAt(right) - 'a']++;
+
+            // Maintain window size equal to s1 length
+            if ((right - left + 1) > s1.length()) {
+                window[s2.charAt(left) - 'a']--;
+                left++;
+            }
+
+            // Compare both frequency arrays
+            if (Arrays.equals(s1Freq, window)) {
+                return true;
+            }
+        }
+        return false;
+
+
+
+
+     
         //T:O(n),S:O(1)
         //s1 cannot be greater than s2 
         //because if s1 is longer, no permutation of s1 can exist in s2 
