bdae7e63c57fac0a569a283e69ec416ec17fd04e
AkshithArelli <arelliakshith07@gmail.com>
2025-11-22 21:03:30 +0530

    Update 70. Climbing Stairs.java

--- 70. Climbing Stairs.java
+++ 70. Climbing Stairs.java
@@ -3,7 +3,7 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
  
-
+```
 Example 1:
 
 Input: n = 2
@@ -24,8 +24,8 @@ Explanation: There are three ways to climb to the top.
 Constraints:
 
 1 <= n <= 45
-
-
+```
+```java
 class Solution {
     public int climbStairs(int n) {
         //for 1 step: 1way
@@ -70,3 +70,4 @@ class Solution {
         return climbStairs(n-1) + climbStairs(n-2);
     }
 } 
+```
